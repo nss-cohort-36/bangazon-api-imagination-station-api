0f7705be24339e036fbcecc7614c5228b12313d9
Ryan Crowley <ryancrowleycode@gmail.com>
2020-02-21 10:17:07 -0600

    created the orders.py model and imported it into the package for models

---
+++ bangazonapi/models/orders.py
@@ -0,0 +1,30 @@
+from django.db import models
+from .customers import Customer
+from .paymenttypes import PaymentType
+
+class Order(models.Model):
+    """
+    This class is responsible for creating the order instances.
+
+    Author: 
+        Ryan Crowley
+    """
+
+    customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
+    payment_type = models.ForeignKey(PaymentType, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    class Meta:
+        ordering = ("-created_at", )
+        verbose_name = ("order")
+        verbose_name_plural = ("orders")
+
+    def __str__(self):
+        return f'''
+        Order: {self.id} 
+        Customer: {self.customer.user.first_name} {self.customer.user.last_name}
+        '''
+
+    
+
+
