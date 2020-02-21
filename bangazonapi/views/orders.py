cf7cae02becb4d6ae740d0f63704e231754111c3
Ryan Crowley <ryancrowleycode@gmail.com>
2020-02-21 13:54:10 -0600

    working on orders view

---
+++ bangazonapi/views/orders.py
@@ -0,0 +1,6 @@
+"""View module for handling requests about orders"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from bangazonapi.models import Order
