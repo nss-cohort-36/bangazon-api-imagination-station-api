1836d0243e8cc0214df24a2b850fbfd0e8dc483d
Ryan Crowley <ryancrowleycode@gmail.com>
2020-02-21 14:03:14 -0600

    serializer for order view has been completed

--- bangazonapi/views/orders.py
+++ bangazonapi/views/orders.py
@@ -4,3 +4,21 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from bangazonapi.models import Order
+
+class OrderSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for park areas
+
+    Arguments:
+        serializers.HyperlinkedModelSerializer
+    """
+
+    class Meta:
+        model = Order
+        # creates a clickable url for the API
+        url = serializers.HyperlinkedIdentityField(
+            view_name="order",
+            lookup_field='id'
+        )
+        fields = ('id', 'created_at', 'customer', 'payment_type')
+
+        
