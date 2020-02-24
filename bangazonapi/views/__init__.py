74f4d61504da2e85425f2601590bdbe06b0828f3
Trey Suiter <trey.suiter@gmail.com>
2020-02-24 11:56:03 -0600

    updated init py for views

--- bangazonapi/views/__init__.py
+++ bangazonapi/views/__init__.py
@@ -3,6 +3,6 @@ from .register import register_user
 from .register import login_user
 from .products import Products
 from .orders import Orders
-from .customers import Customers, Users
+from .customers import Customers, Users, CustomersSerializer
 from .orderproducts import OrderProducts
 from .paymenttypes import PaymentTypes
