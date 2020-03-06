6a3903d3904fd7bfb79e5dad7cfdbaf827a0487e
Manila Bui <manila.bui@gmail.com>
2020-03-06 15:14:06 -0600

    removed some comments

--- bangazonapi/tests/testOrders.py
+++ bangazonapi/tests/testOrders.py
@@ -54,19 +54,9 @@ class TestOrders(TestCase):
         # Now we can grab all the product types (meaning the one we just created) from the db
         response = self.client.get(reverse('order-list'), HTTP_AUTHORIZATION='Token ' + str(self.token))
 
-        # Check that the response is 200 OK.
-        # This is checking for the GET request result, not the POST. We already checked that POST works in the previous test!
         self.assertEqual(response.status_code, 200)
-
-        # response.data is the python serialised data used to render the JSON, while response.content is the JSON itself.
-        # Are we responding with the data we asked for? There's just one product type in our dummy db, so it should contain a list with one instance in it
         self.assertEqual(len(response.data), 1)
-
-        # test the contents of the data before it's serialized into JSON
         self.assertEqual(response.data[0]["customer"]["id"], 1)
-
-        # Finally, test the actual rendered content as the client would receive it.
-        # .encode converts from unicode to utf-8. Don't get hung up on this. It's just how we can compare apples to apples
         self.assertIn(new_order.customer.encode(), response.content)
 
 
